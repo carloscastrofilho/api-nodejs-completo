7fe31f8730d98f33c34d919aad85018f7d6c743e
Carlos Pereira de Castro Filho <50716871+carloscastrofilho@users.noreply.github.com>
2024-10-04 19:07:09 -0300

    routes-add-ok

--- back/src/routes.ts
+++ back/src/routes.ts
@@ -1,1 +1,8 @@
 // controlar as rotas da nossa api
+import fastify, { FastifyInstance , FastifyPluginOptions, FastifyRequest, FastifyReply} from "fastify"
+
+export async function routers( fastify: FastifyInstance, options: FastifyPluginOptions){
+    fastify.get("/teste", async (request:FastifyRequest, reply:FastifyReply)=>{
+        return {ok: true}
+    })
+}
--- back/src/server.ts
+++ back/src/server.ts
@@ -2,12 +2,15 @@
 
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { routers } from "./routes";
 
 const app = Fastify({logger: true});
 
 const start = async () => {
+    await app.register(cors);
+    await app.register(routers);
     try {
-        await app.listen({port: 333});
+        await app.listen({port: 3333});
     } catch (err) {
         process.exit(1);
         
